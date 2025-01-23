6ffac09c91679b3e7f399965581769d8b03f2c41
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2025-01-23 11:02:54 -0500

    add

--- server.js
+++ server.js
@@ -1,55 +1,3 @@
-// const jsonServer = require('json-server');
-// const path = require('path');
-// const jwt = require('jsonwebtoken');
-//
-// const server = jsonServer.create();
-// const router = jsonServer.router(path.join(__dirname, 'db.json'));
-// const middlewares = jsonServer.defaults();
-//
-// // Enable CORS for all responses
-// server.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//     next();
-// });
-//
-// server.use(middlewares);
-// server.use(jsonServer.bodyParser);
-//
-// const SECRET_KEY = 'your_secret_key';
-// const expiresIn = '1h';
-//
-// // Create a token from a payload
-// function createToken(payload) {
-//     return jwt.sign(payload, SECRET_KEY, { expiresIn });
-// }
-//
-// // Check if the user exists in the database
-// function isAuthenticated({ username, password }) {
-//     return router.db.get('users').find({ username, password }).value();
-// }
-//
-// server.post('/login', (req, res) => {
-//     const { username, password } = req.body;
-//     const user = isAuthenticated({ username, password });
-//
-//     if (user) {
-//         const token = createToken({ username, userId: user.id });
-//         res.status(200).json({ token });
-//     } else {
-//         res.status(401).json({ error: 'Invalid credentials' });
-//     }
-// });
-//
-// server.use(router);
-//
-// const port = process.env.PORT || 3000;
-// server.listen(port, () => {
-//     console.log(`JSON Server is running on port ${port}`);
-// });
-
-
 const jsonServer = require('json-server');
 const path = require('path');
 const jwt = require('jsonwebtoken');
@@ -57,6 +5,16 @@ const jwt = require('jsonwebtoken');
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
+
+// Serve static files from the "public" folder
+server.use(express.static(path.join(__dirname, 'public')));
+
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 const SECRET_KEY = 'your_secret_key';
 const expiresIn = '1h';
